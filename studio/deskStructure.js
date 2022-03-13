e6f0438dbb915fff16861912ddb5a1a7c5268232
Owen Dodd <owen.dodd@gmail.com>
2022-03-13 21:47:07 +0100

    update

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,27 +1,8 @@
-import S from "@sanity/desk-tool/structure-builder";
-// Add Schema type to hidden
-const hiddenDocTypes = listItem =>
-  !["page", "siteconfig",].includes(
-    listItem.getId()
-  );
+import S from '@sanity/desk-tool/structure-builder'
 
-// Render a custom UI to view siteconfig & pages 
-// and showing other items except mentioed in the hiddendoctypes
 export default () =>
   S.list()
-    .title("Content Manager")
-    .items([
-      S.listItem()
-        .title("Site config")
-        .icon(HiOutlineCog)
-        .child(
-          S.editor()
-            .schemaType("info")
-            .documentId("info")
-        ),
-      // Add a visual divider (optional)
-      S.divider(),
-      S.documentTypeListItem("page").title("Pages"),
-      S.divider(),
-      ...S.documentTypeListItems().filter(hiddenDocTypes)
-    ]);
+    .title('Base')
+    .items(
+      S.documentTypeListItems()
+    )
