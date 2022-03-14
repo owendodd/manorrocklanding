3ae3fe2810a49645348d96fc143fb89cb0b3ecf6
Owen Dodd <owen.dodd@gmail.com>
2022-03-14 14:09:09 +0100

    update

--- components/grid.js
+++ components/grid.js
@@ -1,7 +1,7 @@
 export default function Grid({ children }) {
     return (
       <>
-        <div className="grid w-full mx-auto grid-cols-3 lg:grid-cols-6 gap-6 p-6 auto-rows-min">
+        <div className="grid w-full mx-auto grid-cols-3 lg:grid-cols-6 gap-x-6 gap-y-3 p-6 auto-rows-min">
             {children}
         </div>
       </>
