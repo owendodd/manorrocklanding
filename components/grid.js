a3ff32ad26b33f6425e599505a7552ea9fef5b0c
Owen Dodd <owen.dodd@gmail.com>
2022-03-15 13:57:55 +0100

    fix padding

--- components/grid.js
+++ components/grid.js
@@ -1,9 +1,7 @@
 export default function Grid(props) {
     return (
-      <>
-        <div className={"grid w-full mx-auto grid-cols-3 lg:grid-cols-6 gap-x-6 gap-y-3 px-6 md:px-32 " + (props.class)}>
-            {props.children}
-        </div>
-      </>
-    )
+      <div className={"grid w-full mx-auto grid-cols-3 lg:grid-cols-6 gap-x-6 gap-y-3 px-6 md:px-32 " + (props.className)}>
+          {props.children}
+      </div>
+  )
   }
