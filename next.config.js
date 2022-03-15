3343ffe0f222373646780027e913fc5c7c435a4d
Owen Dodd <owen.dodd@gmail.com>
2022-03-15 12:29:28 +0100

    fix url for studio

--- next.config.js
+++ next.config.js
@@ -6,4 +6,16 @@ const nextConfig = {
   },
 }
 
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
+module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
+};
+
 module.exports = nextConfig
